3774595b19120eb024627b81da66487cba9f437f
Josue <josuepereztorr@gmail.com>
2022-02-01 18:01:55 -0600

    fixed spacing

--- Program.cs
+++ Program.cs
@@ -6,23 +6,26 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
+
             // input response 
             string resp = Console.ReadLine();
 
             if (resp == "1")
             {
-                // TODO: create data file 
+
                 // create data file 
 
                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
+
                 // input the response (convert to int)
                 int weeks = int.Parse(Console.ReadLine());
+
             }
             else if (resp == "2")
             {
