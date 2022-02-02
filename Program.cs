ceb950e88647206241c4b06fd7bea414fd40523b
Josue <josuepereztorr@gmail.com>
2022-02-01 20:28:52 -0600

    Parse data file: -read file line by line, -save line as a string (1 line = 1 week), -split string into array using delimiter, -display sleep data

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace SleepData
         static void Main(string[] args)
         {
 
+            var file = "data.txt";
+
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
@@ -42,7 +44,7 @@ namespace SleepData
                 Random rnd = new Random();
 
                 // create file 
-                StreamWriter sw = new StreamWriter("data.txt");
+                StreamWriter sw = new StreamWriter(file);
 
                 // fill in dummy data for each week 
                 while (startDate < endDate)
@@ -75,6 +77,47 @@ namespace SleepData
             else if (resp == "2")
             {
                 // TODO: parse data file
+
+                // parse data file if it exists 
+                if (File.Exists(file))
+                {
+
+                    // read data from file 
+                    StreamReader sr = new StreamReader(file);
+
+                    // run this until the stream position is at the end 
+                    while (!sr.EndOfStream)
+                    {
+                        // reads a single line of characters and returns it as a string, 1 line of text represents 1 week of data
+                        string line = sr.ReadLine();
+
+                        // convert the string into an array
+                        string[] week = line.Split(',');
+
+                        // the 1st element in teh array is the date, converts the string into a DateTime object 
+                        DateTime date = DateTime.Parse(week[0]);
+
+                        // the 2nd element is the hours of sleep with a "|" delimiter 
+                        int[] hours = Array.ConvertAll(week[1].Split("|"), int.Parse);
+
+                        // display date for the current week 
+                        Console.WriteLine($"Week of {date:MMM}, {date:dd}, {date:yyy}");
+
+                        // display column headers 
+                        Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}");
+
+                        // display hours of sleep for each day 
+                        Console.WriteLine($"{hours[0],3}{hours[1],3}{hours[2],3}{hours[3],3}{hours[4],3}{hours[5],3}{hours[6],3}");
+                        Console.WriteLine();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist");
+                }
+
             }
         }
     }
